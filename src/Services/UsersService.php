79041f7bf64dcb216c2c7336ae2d3773c08dea03
Mostafa Zareei <mostafaz7474@gmail.com>
2024-09-01 12:24:56 +0330

    add user service class for handling operation on user table

---
+++ src/Services/UsersService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\UserModel;
+
+class UsersService
+{
+    private UserModel $userModel;
+
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
+
+    public function getListOfUsers(): array
+    {
+        return $this->userModel->findAll();
+    }
+
+    public function create(array $data): bool
+    {
+        [$name, $email, $password] = [$data['name'], $data['email'], $data['password']];
+
+        $password = passwordHash($password, PASSWORD_BCRYPT);
+
+        return $this->userModel->create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+        ]);
+    }
+
+    public function update(int $userId, array $data): bool
+    {
+        [$name, $email] = [$data['name'], $data['email']];
+
+        return $this->userModel->update($userId, [
+            'name' => $name,
+            'email' => $email,
+        ]);
+    }
+
+    public function delete(int $userId): bool
+    {
+        return $this->userModel->delete($userId);
+    }
+}
