80e4e26eb9facd00b1c67a1edc0d609e25c2f694
Mostafa Zareei <mostafaz7474@gmail.com>
2024-09-01 14:30:44 +0330

    add logic to users controller for handling api request

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -16,12 +16,61 @@ class UserController
     }
 
     public function list(Request $request)
-    {   
-        Response::respondAndDie([]);
+    {
+        $users = $this->userService->getListOfUsers();
+
+        Response::respondAndDie($users);
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        echo "users create resource";
+        $data = $request->params();
+
+        [$userName, $userEmail, $password] = [$data['name'], $data['email'], $data['password']];
+
+        $isCreated = $this->userService->create([
+            'name' => $userName,
+            'email' => $userEmail,
+            'password' => $password
+        ]);
+
+        if (!$isCreated) {
+            Response::respondAndDie([], Response::HTTP_NOT_FOUND);
+        }
+
+        Response::respondAndDie(['User Created.']);
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->params();
+        
+        [$userId, $userName, $userEmail] = [$data['id'], $data['name'], $data['email']];
+
+        $isUpdated = $this->userService->update($userId, [
+            'name' => $userName,
+            'email' => $userEmail,
+        ]);
+
+        if (!$isUpdated) {
+            Response::respondAndDie([], Response::HTTP_NOT_FOUND);
+        }
+
+        Response::respondAndDie(['User Updated.']);
+    }
+
+    public function delete(Request $request)
+    {
+        $data = $request->params();
+        
+        [$userId] = [$data['id']];
+
+        $isDeleted = $this->userService->delete($userId);
+
+        if (!$isDeleted) {
+            Response::respondAndDie([], Response::HTTP_NOT_FOUND);
+        }
+
+        Response::respondAndDie(['User Deleted.']);
     }
 }
